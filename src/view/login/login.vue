c873c5b43fab6571d932af0785263874eeb6afc9
chenwenjie <593261107@qq.com>
2020-04-05 15:00:58 +0800

    登录界面-登录模块-logo行

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="login">
-    <div class="left"></div>
+    <div class="left">
+      <div class="title">
+        <img src="@/assets/img/login_logo.png" alt />
+        <span class="titleName">黑马面面</span>
+        <span class="titlelist">|</span>
+        <span class="titleName2">用户登录</span>
+      </div>
+    </div>
     <div class="right">
       <img src="@/assets/img/login_banner_ele.png" alt />
     </div>
@@ -28,7 +35,27 @@ export default {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
-  };
-  
+    padding: 43px;
+
+    .titleName {
+      font-size: 24px;
+      font-family: SourceHanSansCN;
+      font-weight: 400;
+      color: rgba(12, 12, 12, 1);
+      padding: 0 15px;
+    }
+    .titlelist {
+      width: 1px;
+      height: 28px;
+      background: rgba(199, 199, 199, 1);
+    }
+    .titleName2 {
+      font-size: 24px;
+      font-family: PingFangSC;
+      font-weight: 400;
+      color: rgba(12, 12, 12, 1);
+      padding-left: 15px;
+    }
+  }
 }
 </style>
