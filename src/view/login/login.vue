79d5a64ceca31274ebda639b80c4a696c8571f5a
chenwenjie <593261107@qq.com>
2020-04-06 16:14:13 +0800

    登录界面-登录模块-完成1

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -7,17 +7,17 @@
         <span class="titlelist">|</span>
         <span class="titleName2">用户登录</span>
       </div>
-
-      <el-form ref="form" :model="form">
-        <el-form-item>
+      <!-- form表单 -->
+      <el-form ref="form"  :model="form" :rules="rules">
+        <el-form-item prop="phone">
           <el-input
-            v-model="form.user"
-            class="inputUser"
+            v-model="form.phone"
+            class="inputPhone"
             prefix-icon="el-icon-user"
             placeholder="请输入手机号"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             class="inputWord"
             prefix-icon="el-icon-lock"
@@ -26,31 +26,31 @@
             v-model="form.password"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-row>
             <el-col :span="16">
               <el-input
-                class="inputCap"
+                class="inputCode"
                 prefix-icon="el-icon-key"
                 v-model="form.code"
                 placeholder="请输入验证码"
               ></el-input>
             </el-col>
             <el-col :span="8">
-              <img src alt />
+              <img src="@/assets/img/key.jpg" class="keyImg" alt />
             </el-col>
           </el-row>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox v-model="checked">
+        <el-form-item prop="checked">
+          <el-checkbox v-model="form.checked">
             我已阅读并同意
-            <el-link type="primary">用户协议</el-link>和
-            <el-link type="primary">隐私条款</el-link>
+            <el-link type="primary" class="link">用户协议</el-link>和
+            <el-link type="primary" class="link">隐私条款</el-link>
           </el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button class="my-btn" type="primary">登录</el-button>
-          <br>
+          <el-button class="my-btn" @click="loginBtn" type="primary">登录</el-button>
+          <br />
           <el-button class="my-btn" type="primary">注册</el-button>
         </el-form-item>
       </el-form>
@@ -68,12 +68,41 @@ export default {
   data() {
     return {
       form: {
-        user: "",
+        phone: "",
         password: "",
-        code: ""
+        code: "",
+        checked: '',
       },
-      checked: false
+      
+      rules:{
+        phone:[
+          {required:true,message:'请输入手机号',trigger:'blur'},
+          {min:11,max:11,message:'请输入11位手机号',trigger:'blur'}
+        ],
+        password:[
+          {required:true,message:'请输入密码',trigger:'blur'},
+          {min:6,max:12,message:'请输入6到12位密码',trigger:'blur'}
+        ],
+        code:[
+          {required:true,message:'请输入验证码',trigger:'blur'},
+          {min:4,max:4,message:'请输入正确验证码',trigger:'blur'}
+        ],
+        checked:[
+          {type: '',required:true,message:'请勾选协议',trigger:'change'},
+        ],
+      }
     };
+  },
+  methods:{
+    loginBtn(){
+      this.$refs.form.validate(valid=>{
+        if(valid){
+           this.$message.success("登录成功")
+        }else{
+          this.$message.error('登录失败');
+        }
+      });
+    }
   }
 };
 </script>
@@ -99,12 +128,12 @@ export default {
       height: 40px;
       background: rgba(20, 147, 250, 1);
       border-radius: 4px;
-    //   margin-top: 28px;
-    }.my-btn:nth-child(3) {
-       margin-top: 26px;
-      
+      //   margin-top: 28px;
     }
-    .inputUser {
+    .my-btn:nth-child(1) {
+      margin-bottom: 26px;
+    }
+    .inputPhone {
       width: 394px;
       height: 42px;
       //   background: rgba(255, 255, 255, 1);
@@ -145,12 +174,22 @@ export default {
       }
     }
 
-    .inputCap {
+    .inputCode {
       width: 284px;
       height: 42px;
       background: rgba(255, 255, 255, 1);
       border: 1px solid rgba(204, 207, 213, 1);
       border-radius: 4px;
+    }
+    .keyImg {
+      margin-left: 10px;
+      width: 120px;
+      height: 42px;
+    }
+
+    .link{
+      margin-top: -3px;
+      margin-left: -3px;
     }
   }
 }
